928be2550ed4d55f9e2fef50a82a164a8804a62b
Vitor Domingues <vtrshaft@gmail.com>
2025-04-26 16:23:14 -0300

    Joystick control of both stepper motors with differential and axial rotation

--- robonejoESP32/src/main.cpp
+++ robonejoESP32/src/main.cpp
@@ -1,90 +1,83 @@
 #include <Arduino.h>
 #include <Ps3Controller.h>
 
-#define PUL 12
-#define DIR 14
+// Pinos do motor esquerdo
+#define PUL_ESQ 12
+#define DIR_ESQ 14
 
+// Pinos do motor direito
+#define PUL_DIR 26
+#define DIR_DIR 27
 
-void motor(bool dir, int vel){
-    digitalWrite(DIR, dir);
-    //delayMicroseconds(200);
-    digitalWrite(PUL, HIGH);
-    delayMicroseconds(1000-vel);
-    digitalWrite(PUL, LOW);
-    delayMicroseconds(1000-vel);
+// Variáveis de controle
+int velocidadeEsq = 0;
+int velocidadeDir = 0;
+
+// Função para controlar motor individual
+void motor(int pulPin, int dirPin, bool dir, int vel) {
+    digitalWrite(dirPin, dir);
+    digitalWrite(pulPin, HIGH);
+    delayMicroseconds(1000 - vel);
+    digitalWrite(pulPin, LOW);
+    delayMicroseconds(1000 - vel);
 }
 
-// Variável para armazenar o comando atual
-int motorCommand = 0;
-int y = 0;
-
-// Callback quando o botão do controle é pressionado
+// Callback quando houver evento do PS3
 void onPs3Event() {
-    // Comando de velocidade
+    int ly = Ps3.data.analog.stick.ly;  // Frente/Trás
+    int lx = Ps3.data.analog.stick.lx;  // Curva
+    int rx = Ps3.data.analog.stick.rx;  // Rotação axial
 
-    if (Ps3.data.button.up) {
-        motorCommand = 100; // Velocidade positiva
-        motor(HIGH,500);
-    } else if (Ps3.data.button.down) {
-        motorCommand = -100; // Velocidade negativa
-        motor(LOW,500);
-    } else {
-        motorCommand = 0; // Parado
+    // Inicializa velocidades baseadas no LY
+    int baseSpeed = map(ly, -128, 127, -600, 600);  // mapa de -600 a 600
+
+    // Curvas diferenciais (LX)
+    int curvaAdjust = map(lx, -128, 127, -300, 300); // diminui/ajusta motor para curva
+
+    // Rotação axial (RX)
+    int rotacao = map(rx, -128, 127, -600, 600);
+
+    // Se o stick direito estiver mexido (rotação pura), ignora LY e LX
+    if (abs(rx) > 10) {
+        velocidadeEsq = rotacao;
+        velocidadeDir = -rotacao;
+    }
+    else {
+        // Stick esquerdo para mover normal e fazer curvas
+        velocidadeEsq = baseSpeed + curvaAdjust;
+        velocidadeDir = baseSpeed - curvaAdjust;
     }
 
-    // Envia comando pela Serial
-    //Serial.println(motorCommand);
-
-    //if( abs(Ps3.event.analog_changed.stick.lx) + abs(Ps3.event.analog_changed.stick.ly) > 2 ){
-        //Serial.print("Moved the left stick:");
-        //Serial.print(" x="); Serial.print(Ps3.data.analog.stick.lx, DEC);
-        //Serial.print(" y="); Serial.print(Ps3.data.analog.stick.ly, DEC);
-        y = map(int(Ps3.data.analog.stick.ly), -130, 130, -600, 600);
-        //Serial.println();
-        Serial.println(y);
-        /*
-        while(y<0){
-            motor(HIGH,y);
-        }
-        while(y>0){
-            motor(LOW,y);
-        }
-        while(y == 0){
-            Serial.println("motor parado");
-        }
-        */
-     //}
- /*
-    //if( abs(Ps3.event.analog_changed.stick.rx) + abs(Ps3.event.analog_changed.stick.ry) > 2 ){
-        Serial.print("Moved the right stick:");
-        Serial.print(" x="); Serial.print(Ps3.data.analog.stick.rx, DEC);
-        Serial.print(" y="); Serial.print(Ps3.data.analog.stick.ry, DEC);
-        int x = map(int(Ps3.data.analog.stick.ry), -130, 130, 0, 900);
-        Serial.println();
-        Serial.println(x);
-        motor(LOW,x);
-    //}
-    */
+    // (Opcional) Debug:
+    Serial.print("LY: "); Serial.print(ly);
+    Serial.print(" LX: "); Serial.print(lx);
+    Serial.print(" RX: "); Serial.print(rx);
+    Serial.print(" | VEL_ESQ: "); Serial.print(velocidadeEsq);
+    Serial.print(" VEL_DIR: "); Serial.println(velocidadeDir);
 }
-    
 
 void setup() {
-    pinMode(DIR, OUTPUT);
-    pinMode(PUL, OUTPUT);
-    Serial.begin(115200); // Comunicação com Arduino
+    pinMode(DIR_ESQ, OUTPUT);
+    pinMode(PUL_ESQ, OUTPUT);
+    pinMode(DIR_DIR, OUTPUT);
+    pinMode(PUL_DIR, OUTPUT);
+
+    Serial.begin(115200);
     Ps3.begin();
     Ps3.attach(onPs3Event);
     Serial.println("Controle PS3 conectado!");
 }
 
 void loop() {
-    if(y<0){
-        motor(HIGH,-y);
+    // Controle motor esquerdo
+    if (abs(velocidadeEsq) > 10) {  // deadzone pra evitar motor vibrando parado
+        motor(PUL_ESQ, DIR_ESQ, (velocidadeEsq > 0) ? LOW : HIGH, abs(velocidadeEsq));
     }
-    else if(y>0){
-        motor(LOW,y);
+
+    // Controle motor direito
+    if (abs(velocidadeDir) > 10) {
+        motor(PUL_DIR, DIR_DIR, (velocidadeDir > 0) ? LOW : HIGH, abs(velocidadeDir));
     }
-    else{
-        Serial.println("motor parado");
-    }
+
+    delay(1);  // Pequeno delay para não sobrecarregar o loop
 }
